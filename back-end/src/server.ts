0aff1e23b86d1bf1d994b99358d965a00f693a14
Odair Michael Bendotti <obendotti@gmail.com>
2026-01-22 18:48:43 -0300

    feat: add frontend fallback routing

--- back-end/src/server.ts
+++ back-end/src/server.ts
@@ -1,12 +1,27 @@
 import express from "express";
+import { fileURLToPath } from "url";
 import { authRoutes } from "./routes/auth.js";
 import { messagesRouter } from "./routes/messages.js";
+import path from "path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 
-// Routes using
+// Routes
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messagesRouter);
+
+// Relative path to production
+if (process.env.NODE_ENV === "development") {
+  const static_frontend = path.join(__dirname, "../../front-end/dist");
+  app.use(express.static(static_frontend));
+
+  app.use((_, res) => {
+    res.sendFile(path.join(static_frontend, "index.html"));
+  });
+}
 
 app.listen(process.env.PORT, () => {
   console.log("Server running on http://localhost:" + process.env.PORT);
